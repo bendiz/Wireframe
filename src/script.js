fd5ea5d6fe1f6dbd04362e5574695ab63148accf
Bendiz <benerok@icloud.com>
2022-07-10 22:27:44 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,122 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+
+// Variables
+// const citySelection = prompt("Enter a city").trim().toLowerCase();
+let message = "";
+let celsius = 0;
+let fahrenheit = 0;
+let humidity = 0;
+let upperCaseLetter = "";
+let lowerCaseLetters = "";
+let city = "";
+
+// // Converts celsius into fahrenheit
+function celsiusToFahrenheit(celsius) {
+  fahrenheit = (celsius *= 1.8) + 32;
+  return Math.round(fahrenheit);
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  celsius = ((fahrenheit - 32) * 5) / 9;
+  return Math.round(celsius);
+}
+
+// If the city exists in the object - display a rounded number of temp and humidity in both celsius and fahrenheit.
+// Else redirect the user to search for the user inputted city in google.
+function weatherMessage(citySelection) {
+  if (weather[citySelection] !== undefined) {
+    celsius = Math.round(weather[citySelection].temp);
+    celsiusToFahrenheit(celsius);
+    humidity = weather[citySelection].humidity;
+    message = `It´s currently ${celsius}°C (${fahrenheit}°F) in ${lowerToUpperCase(
+      citySelection
+    )} with a humidity of ${humidity}%`;
+    alert(message);
+  } else
+    alert(
+      `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${citySelection}`
+    );
+}
+
+// Make first letter of city capitalized for the weather alert message
+function lowerToUpperCase(citySelection) {
+  upperCaseLetter = citySelection.charAt(0).toUpperCase();
+  lowerCaseLetters = citySelection.substring(1);
+  city = upperCaseLetter.concat(lowerCaseLetters);
+  return city;
+}
+
+// weatherMessage(citySelection);
+
+// Homework week 4
+// Feature 1 - display current day and time
+let date = new Date();
+
+let currentDayText = document.querySelector("#current-day");
+let numberOfDay = date.getDay();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let currentDay = days[numberOfDay];
+currentDayText.innerHTML = currentDay.toUpperCase();
+
+// Feature 2 - Add search engine
+let cityNameText = document.querySelector("#current-city");
+let form = document.querySelector("#search-form");
+function displayCity(event) {
+  event.preventDefault();
+  let cityInput = document.getElementById("search").value.toLowerCase().trim();
+  cityNameText.innerHTML = `${lowerToUpperCase(cityInput)}`;
+}
+form.addEventListener("submit", displayCity);
+
+// Bonus Feature
+let temperatureLink = document.querySelector("#celsius-fahrenheit");
+let fakeTemperature = document.querySelector("#fake-temperature");
+let fakeFahrenheit = 0;
+let fakeCelsius = document.querySelector("#fake-temperature").innerHTML;
+
+temperatureLink.addEventListener("click", temperatureConversion);
+
+function temperatureConversion() {
+  if (temperatureLink.innerHTML === "°C") {
+    fakeFahrenheit = celsiusToFahrenheit(fakeCelsius);
+    temperatureLink.innerHTML = "°F";
+    fakeTemperature.innerHTML = fakeFahrenheit;
+    return fakeFahrenheit;
+  } else if (temperatureLink.innerHTML === "°F") {
+    fakeCelsius = fahrenheitToCelsius(fakeFahrenheit);
+    temperatureLink.innerHTML = "°C";
+    fakeTemperature.innerHTML = fakeCelsius;
+    return fakeCelsius;
+  }
+}
